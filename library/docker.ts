acbc98ebb7f18f8b9334397d157eed643692f012
Tim Docker <tim@dockerz.net>
2019-02-11 18:11:02 +1100

    Update docker daemon config to set default container log size to 100M
    Reviewers: barry
    
    Subscribers: #helix_omniscient
    
    Differential Revision: http://phab.helixta.com.au/D22234

--- library/docker.ts
+++ library/docker.ts
@@ -6,7 +6,11 @@ export interface DockerConfig {
 }
 
 export function daemonConfig(cfg: DockerConfig): string {
-  return JSON.stringify({ 'log-driver': 'json-file' }, null, 2);
+  return JSON.stringify({ 
+    'log-driver': 'json-file' ,
+    'log-opts': {
+      'max-size': '100m'
+    }}, null, 2);
 }
 
 export const DEFAULT_CONFIG: DockerConfig = {};

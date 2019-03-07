e04fe2ad4737be051feecc488097ce358be24508
Tim Docker <tim@dockerz.net>
2019-03-07 11:59:40 +1100

    Pick up bugfix for hx-deploy-tool
    Reviewers: dong
    
    Subscribers: #helix_omniscient
    
    Differential Revision: http://phab.helixta.com.au/D22703

--- library/deploytool/releaseurl.ts
+++ library/deploytool/releaseurl.ts
@@ -1,1 +1,1 @@
-export const release_url: string = "https://github.com/helix-collective/hx-deploy-tool/releases/download/0.13/hx-deploy-tool.x86_64-linux.gz -O /opt/bin/hx-deploy-tool.gz";
+export const release_url: string = "https://github.com/helix-collective/hx-deploy-tool/releases/download/0.13.1/hx-deploy-tool.x86_64-linux.gz -O /opt/bin/hx-deploy-tool.gz";

5bf1b8f0d89c7592b8d60fce721149dd8bd99691
Tim Docker <tim@dockerz.net>
2019-03-11 14:12:09 +1100

    New function to create a randomly initialized secret
    Reviewers: dong
    
    Subscribers: #helix_omniscient
    
    Differential Revision: http://phab.helixta.com.au/D22759

--- library/aws/secrets.ts
+++ library/aws/secrets.ts
@@ -2,6 +2,7 @@ import * as TF from '../../core/core';
 import * as AR from '../../providers/aws/resources';
 import _ from 'lodash';
 import { ArnT } from '../../providers/aws/types';
+import { SharedResources } from './shared';
 import { DESTRUCTION } from 'dns';
 
 export type ArnSecret = ArnT<'SecretsmanagerSecret'>;
@@ -29,4 +30,32 @@ export function createJsonSecret(tfgen: TF.Generator, name: string, params: Json
     });
 
     return secret;
+}
+
+/**
+ * Create a JSON secret in the AWS Secrets manager, initialized with a random string.
+ * 
+ * The secret will have structure:
+ * 
+ *    { "secret", "XXXXXX" }
+ * 
+ */
+export function createRandomJsonSecret(tfgen: TF.Generator, name: string, sr: SharedResources, params: JsonSecretParams) {
+  const secret = AR.createSecretsmanagerSecret(tfgen, name, {
+    name: tfgen.scopedName(name).join("_").replace(/-/g,"_"),
+    tags: tfgen.tagsContext(),
+    description: params.description
+  });
+  const size = 20;
+  
+  tfgen.localExecProvisioner(
+    secret,
+    [
+      '# Generate a random password for the secrets initial value',
+      `export AWS_REGION=${sr.network.region.value}`,
+      `hx-provisioning-tools generate-password --size ${size} --to-secret ${secret.arn.value}`,
+    ].join('\n')
+  );
+
+  return secret;
 }

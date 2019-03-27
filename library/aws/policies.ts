6eec8b8001d51494395335353aeaa321fb57a647
Tim Docker <tim@dockerz.net>
2019-03-27 20:33:33 +1100

    Added policy for running a lambda function inside a vpc
    Reviewers: jeeva
    
    Subscribers: #helix_omniscient
    
    Differential Revision: http://phab.helixta.com.au/D23074

--- library/aws/policies.ts
+++ library/aws/policies.ts
@@ -247,6 +247,23 @@ export const ecr_modify_all_policy: NamedPolicy = {
   },
 };
 
+export const lambda_in_vpc_policy: NamedPolicy = {
+  name: 'vpclambda',
+  policy: {
+    Version: '2012-10-17',
+    Statement: [
+      {
+        Effect: 'Allow',
+        Action: [
+          "ec2:CreateNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DeleteNetworkInterface",
+        ],
+        Resource: '*',
+      },
+    ],  }
+}
+
 export function edModifyPolicy(
   name: string,
   esdomain: AR.ElasticsearchDomain

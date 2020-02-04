1cfe88fafbee4cac39089757a90369ce5f87b7b0
Paul Thompson <paul@helixta.com.au>
2020-02-04 19:49:00 +1100

    Upgrade to c2 0.15.4

--- library/deploytool/releaseurl.ts
+++ library/deploytool/releaseurl.ts
@@ -1,1 +1,1 @@
-export const release_url: string = "https://github.com/helix-collective/camus2/releases/download/0.15.3/camus2.x86_64-linux.gz -O /opt/bin/camus2.gz";
+export const release_url: string = "https://github.com/helix-collective/camus2/releases/download/0.15.4/camus2.x86_64-linux.gz -O /opt/bin/camus2.gz";

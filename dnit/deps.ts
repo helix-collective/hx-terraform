8686f41426c3c31cc9a33f9a74126734bcc69dd8
Paul Thompson <paul@helixta.com.au>
2020-12-09 08:18:53 +1100

    fix: dnit: Update import url for jszip
    workaround for error:
    
    Warning std versions prefixed with 'v' were deprecated recently. Please change your import to https://deno.land/std@0.61.0/fs/mod.ts (at https://deno.land/std@v0.61.0/fs/mod.ts)
    error: Import 'https://deno.land/std@v0.61.0/fs/mod.ts' failed: 404 Not Found
        at https://deno.land/x/jszip@0.7.0/mod.ts:2:0
    
    jszip 0.8.0 from https://github.com/hayd/deno-zip/blob/0.8.0/mod.ts
    isn't released on https://deno.land/x/jszip yet.
    
    Using https://denopkg.com pulls it straight from github

--- dnit/deps.ts
+++ dnit/deps.ts
@@ -24,6 +24,6 @@ export {
 } from 'https://denopkg.com/helix-collective/dnit-utils@v1.2.0/mod.ts';
 
 // other deno typescript libs:
-export * as jszip from 'https://deno.land/x/jszip@0.7.0/mod.ts';
+export * as jszip from 'https://denopkg.com/hayd/deno-zip@0.8.0/mod.ts';
 
 export * as changeCase from 'https://deno.land/x/case@v2.1.0/mod.ts';

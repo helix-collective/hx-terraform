808bf934989760f9c4d4f0abb5663cd266d9c168
Jon Calderín Goñi <jon.caldering@gmail.com>
2022-09-19 09:12:30 +1000

    ignore all ec2 tags (#139)
    * ignore all ec2 tags
    
    * added comments
    
    Co-authored-by: Jon Calderin Goni <jon@neara.com>

--- library/aws/aws.ts
+++ library/aws/aws.ts
@@ -108,10 +108,13 @@ export function createInstance(
   if(ignoreUserDataChanges) {
     tfgen.ignoreChanges(ec2, 'user_data');
   }
+  // It seems named tags like in the documentation doesn't work
+  // TODO: (jon) open a ticket to terraform 
+  tfgen.ignoreChanges(ec2, "tags")
   // Ignore changes in the following tags
-  params0.tags_to_ignore?.forEach((tag) => {
-    tfgen.ignoreChanges(ec2, `tags.${tag}`)
-  })
+  // params0.tags_to_ignore?.forEach((tag) => {
+  //   tfgen.ignoreChanges(ec2, `tags.${tag}`)
+  // })
 
   return ec2;
 }
